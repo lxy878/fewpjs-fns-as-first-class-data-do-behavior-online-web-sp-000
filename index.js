e3c579e82da369d36a0cb763997745aac85a6c68
Xiaoyi Liu <lxy878@gmail.com>
2020-08-10 16:32:50 +0000

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,18 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time='00:00'){
+  let hour = parseInt(time.split(':')[0])
+  if(hour<12){
+    return "Good Morning"
+  }else if (hour>17) {
+    return "Good Evening"
+  }else{
+    return "Good Afternoon"
+  }
+}
+
+function displayMessage(text){
+  const h1 = document.querySelector('h1#greeting')
+  h1.innerText = text
+}
