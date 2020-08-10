a6500108517aa0115c5664a5e019b11ef0901daf
Xiaoyi Liu <lxy878@gmail.com>
2020-08-10 16:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function handleClick(e) {
 
 
 function greet(time='00:00'){
-  let hour_string = time.split(':')[0]
-  let
+  let hour_string = parseInt(ime.split(':')[0])
   if(times[0].parseInt<12){
     return "Good Morning"
   }else if (times[0].parseInt>19) {
